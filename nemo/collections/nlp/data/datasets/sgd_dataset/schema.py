2e155a21b9aea61ae0ab30ae2da10e9f518bed88
Vahid Noroozi <VahidooX@users.noreply.github.com>
2020-04-17 18:35:52 -0700

    make print to logging.
    Signed-off-by: Vahid Noroozi <VahidooX@users.noreply.github.com>

--- nemo/collections/nlp/data/datasets/sgd_dataset/schema.py
+++ nemo/collections/nlp/data/datasets/sgd_dataset/schema.py
@@ -121,7 +121,7 @@ class ServiceSchema(object):
     def get_categorical_slot_value_id(self, slot, value):
         # debugging info
         if value not in self._categorical_slot_value_ids[slot]:
-            print(f"Not found value:{value} for slot:{slot} in {self._service_name}")
+            logging.info(f"Not found value:{value} for slot:{slot} in {self._service_name}")
             return -1
         return self._categorical_slot_value_ids[slot][value]
 
