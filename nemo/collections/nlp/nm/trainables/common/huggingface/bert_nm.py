41af95ecccaa72039247a2868db89f63397a53c9
Vahid Noroozi <VahidooX@users.noreply.github.com>
2020-05-04 15:09:37 -0700

    Added attention_mask to data utils and data layers.
    Signed-off-by: Vahid Noroozi <VahidooX@users.noreply.github.com>

--- nemo/collections/nlp/nm/trainables/common/huggingface/bert_nm.py
+++ nemo/collections/nlp/nm/trainables/common/huggingface/bert_nm.py
@@ -60,7 +60,7 @@ class BERT(TrainableNM):
         return {
             "input_ids": NeuralType(('B', 'T'), ChannelType()),
             "token_type_ids": NeuralType(('B', 'T'), ChannelType()),
-            "attention_mask": NeuralType((), ChannelType()),
+            "attention_mask": NeuralType(), #changed here
             "position_ids": NeuralType(('B', 'T'), ChannelType(), optional=True),
         }
 
