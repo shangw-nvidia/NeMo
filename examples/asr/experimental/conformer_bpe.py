cc3d956df43a8050ecdf0d2a4498ee360e052b9f
Shang Wang <shangw@nvidia.com>
2020-08-21 12:53:38 -0400

    Commenting out wandb callback.

--- examples/asr/experimental/conformer_bpe.py
+++ examples/asr/experimental/conformer_bpe.py
@@ -358,17 +358,17 @@ def create_all_dags(args, neural_factory):
             wandb_name = None
             wandb_project = None
 
-        eval_callback = nemo.core.EvaluatorCallback(
-            eval_tensors=[loss_e, predictions_e, transcript_e, transcript_len_e,],
-            user_iter_callback=partial(process_evaluation_batch, tokenizer=tokenizer),
-            user_epochs_done_callback=partial(process_evaluation_epoch, tag=tagname, eval_metric='WER'),
-            eval_step=args.eval_freq,
-            tb_writer=neural_factory.tb_writer,
-            wandb_name=wandb_name,
-            wandb_project=wandb_project
-        )
+        #eval_callback = nemo.core.EvaluatorCallback(
+        #    eval_tensors=[loss_e, predictions_e, transcript_e, transcript_len_e,],
+        #    user_iter_callback=partial(process_evaluation_batch, tokenizer=tokenizer),
+        #    user_epochs_done_callback=partial(process_evaluation_epoch, tag=tagname, eval_metric='WER'),
+        #    eval_step=args.eval_freq,
+        #    tb_writer=neural_factory.tb_writer,
+        #    wandb_name=wandb_name,
+        #    wandb_project=wandb_project
+        #)
 
-        callbacks.append(eval_callback)
+        #callbacks.append(eval_callback)
 
     return loss_t, callbacks, steps_per_epoch
 
